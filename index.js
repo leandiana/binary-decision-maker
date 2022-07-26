e973989d232dae64616b22223fca11bbfa4d238f
Leandro <leandrofdiana@gmail.com>
2022-07-26 13:15:36 -0300

    add public folder and index.html

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 http.createServer( (request, response)=>{
+    if(request.url === '/'){
+        fs.readFile(
+            path.join(__dirname, 'public', 'index.html'),
+            (err, content)=>{
+                if(err)throw err;
+                response.writeHead(200, {'Content-Type': 'text/html'})
+                response.end(content)
+            }
+            )
+        
+    }
     response.writeHead(200, {'Content-Type': 'text/html'})
-    response.write('Hello World!' /*CONTENT*/)
+    response.write( (Math.random() > 0.5)? '1' : '0' /*CONTENT*/)
     response.end()
     }).listen(8000)
